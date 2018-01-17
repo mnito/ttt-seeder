1eed6a0f3ec92e011d44a26b0fd3aeb3d6c85072
mnito <mpnitowski@gmail.com>
2018-01-16 22:07:30 -0500

    Initial seeding implementation

--- main.cpp
+++ main.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <algorithm>
+#include <functional>
 
 #include "read.hpp"
-#include "parse.hpp"
+#include "score.hpp"
+
 
 int main() {
     std::vector<std::map <std::string, std::string> > data;
     PlayerDataParser parser = PlayerDataParser();
+    PlayerScorer scorer = PlayerScorer();
+    std::vector<PlayerScore> playerscores;
 
     try {
         CsvReader reader = CsvReader("data.csv");
         data = reader.read();
-        reader.read( [&parser](std::map<std::string, std::string> row) {
-            std::cout << row.at("What is your name?") << std::endl;
-            parser.parse(row);
+        reader.read( [&parser, &scorer, &playerscores](std::map<std::string, std::string> row) {
+            PlayerData player = parser.parse(row);
+            unsigned int score = scorer.score(player);
+            PlayerScore ps = PlayerScore();
+            ps.player = player;
+            ps.score = score;
+            playerscores.push_back(ps);
         });
     } catch(const char* e) {
         std::cout << e << std::endl;
     }
 
-    std::cout << "============================BREAK=============================" << std::endl;
+    std::sort(playerscores.begin(), playerscores.end(), std::greater<PlayerScore>());
 
-    for(unsigned int i = 0; i < data.size(); i += 1) {
-        std::cout << data.at(i).at("What is your name?") << std::endl;
+    for(unsigned int i = 0; i < playerscores.size(); i += 1) {
+        PlayerScore ps = playerscores[i];
+        std::cout << (i + 1) << ". " << ps.player.name << " (" << ps.score << ")" << std::endl;
     }
 
     return 0;
